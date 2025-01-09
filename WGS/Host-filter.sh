03b0d952715bc8d03c7275e5d7e42358861ee6b0
diegocoleto7 <150840214+diegocoleto7@users.noreply.github.com>
2025-01-09 13:53:56 +0100

    Create Host-filter.sh

---
+++ WGS/Host-filter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -p bioinfo                      #partition/queue name
+#SBATCH --job-name=Decont-host                #Job name
+#SBATCH -N 1                   #Un nodo
+#SBATCH -n 20                   #Cores
+#SBATCH -t 48:00:00             #Time limit hrs:min:sec
+#SBATCH --output=/home/compubio/log-%j.o #Log de salida
+#SBATCH --error=/home/compubio/log-%j.e #Log de errores
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+
+# Module load
+module load bowtie2/2.5.4
+
+# Directories
+input_dir="/home/proyectos/imdeaalim/compubio/diego/data/WGS"  
+output_dir="/home/proyectos/imdeaalim/compubio/diego/data/filtered_data"  
+genome_dir="home/proyectos/imdeaalim/compubio/genome"
+mkdir -p ${output_dir}
+
+# Filter FASTQ´s
+for fastq_file in ${input_dir}/*.fastq.gz; do
+    base_name=$(basename ${fastq_file} .fastq.gz)
+    output_fastq="${output_dir}/${base_name}_filtered.fastq"
+    bowtie2 -x ${genome_dir}/GRCh38_latest.bowtie-index -U ${fastq_file} --un-gz ${output_fastq}.gz -S /dev/null
+done
